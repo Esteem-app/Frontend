0330bc9d7969ce8a3982a3d171e50b67054a3699
UrszulaP <urszulapoliszuk@gmail.com>
2021-05-28 13:28:38 +0200

    add user settings component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserLoginComponent } from './user/user-login/user-login.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserSettingsComponent } from './user/user-settings/user-settings.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'register', component: UserRegisterComponent },
   { path: 'login', component: UserLoginComponent },
+  { path: 'settings', component: UserSettingsComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
 
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
 
 
 
 @NgModule({
-  declarations: [UserLoginComponent, UserRegisterComponent],
+  declarations: [UserLoginComponent, UserRegisterComponent, UserSettingsComponent],
   imports: [
     CommonModule
   ]
