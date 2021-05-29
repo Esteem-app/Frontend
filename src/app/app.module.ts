7c5c6cda7af36a4d2f30b75404da9481df9b0d57
UrszulaP <urszulapoliszuk@gmail.com>
2021-05-29 14:31:12 +0200

    add logic to adding achievement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
-import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 
@@ -11,6 +10,7 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { HomeFormComponent } from './home-form/home-form.component';
 import { HomeWallComponent } from './home-wall/home-wall.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -18,10 +18,10 @@ import { HomeWallComponent } from './home-wall/home-wall.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
+    FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    // MatGridListModule,
     MatButtonModule,
     MatCardModule
   ]
