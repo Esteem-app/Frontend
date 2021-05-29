f9fe105513f6bebcf0f9b1ffe0f7e793277f1e01
UrszulaP <urszulapoliszuk@gmail.com>
2021-05-29 20:48:26 +0200

    fix url

--- src/app/config/urls.ts
+++ src/app/config/urls.ts
@@ -1,7 +1,7 @@
 export class Urls {
   
-  public static SERVER_URL = 'http://127.0.0.1:8000';
-  // public static SERVER_URL = 'https://good-enough.herokuapp.com';
+  // public static SERVER_URL = 'http://127.0.0.1:8000';
+  public static SERVER_URL = 'https://good-enough.herokuapp.com';
   
   public static REGISTRATION = Urls.SERVER_URL + '/auth/users/';
   public static LOGIN = Urls.SERVER_URL + '/auth/token/login/';
